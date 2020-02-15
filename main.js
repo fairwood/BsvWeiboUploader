9c6003145199d72e6c786fab0470d662e7f4e998
Stephen Zhou <stv1024@gmail.com>
2020-02-15 18:14:12 +0800

    checkpoint

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
+var bsv = require('bsv')
 var NodeAPI = require('./NodeAPI')
-var sec = require('./secret')
-console.log(sec)
-NodeAPI.getUTXOs('12XXBHkRNrBEb7GCvAP4G8oUs5SoDREkVX', (ch) => { console.log(ch) })
+var secret = require('./secret')
+
+// NodeAPI.getUTXOs(secret.Address, (ch) => { console.log(ch) })
 
 
-NodeAPI.getTx('5e3014372338f079f005eedc85359e4d96b8440e7dbeb8c35c4182e0c19a1a12', (ch) => { console.log(ch) })
+//NodeAPI.getTx('5e3014372338f079f005eedc85359e4d96b8440e7dbeb8c35c4182e0c19a1a12', (ch) => { console.log(ch) })
+
+var address = bsv.Address.fromString(secret.Address)
+
+var script = bsv.Script.fromAddress(address)
+
+//console.log(script.toASM())
+// 'OP_DUP OP_HASH160 e2a623699e81b291c0327f408fea765d534baa2a OP_EQUALVERIFY OP_CHECKSIG'
+script = bsv.Script.fromString('14')
+console.log(script.toHex())
+// '76a914e2a623699e81b291c0327f408fea765d534baa2a88ac'
